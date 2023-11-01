9c948289beeb34677dbd7a08c4c1caf2b68d1dd5
Abkels <abrahamk29@gmail.com>
2023-11-01 21:47:41 +0100

    setting up the logger middleware

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,8 +1,10 @@
 import app from './app.js'
+import logger from "./config/logger.config.js"
 
 const PORT = process.env.PORT || 8000
 
 
 app.listen(PORT,()=> {
-    console.log(`server is listening on ${PORT}`)
+    logger.info(`server is listening on ${PORT}`)
 })
+// using the logger created using winston, this replaces console.log
