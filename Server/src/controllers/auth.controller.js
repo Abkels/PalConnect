51a0e41232397376e76a0b137e023a85655e9848
Abkels <abrahamk29@gmail.com>
2023-11-04 20:43:50 +0100

    logout

--- Server/src/controllers/auth.controller.js
+++ Server/src/controllers/auth.controller.js
@@ -75,7 +75,10 @@ export const login = async (req, res, next) => {
 
 export const logout = async (req, res, next) => {
     try {
-        
+        res.clearCookie("refreshToken", {path: "/api/v1/auth/refreshtoken"}); 
+        res.json({
+            message:" logged out"
+        })
     } catch (error) {
            next(error);
     }
