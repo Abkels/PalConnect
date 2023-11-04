5ef6f842229cc90d510fcd67b757daefd3fc39e1
Abkels <abrahamk29@gmail.com>
2023-11-04 21:21:49 +0100

    auth completed with refresh token generation

--- Server/src/services/token.services.js
+++ Server/src/services/token.services.js
@@ -1,8 +1,11 @@
-import {sign} from "../utils/token.utils.js"
+import {sign, verify} from "../utils/token.utils.js"
 
 export const generateToken = async (payload, expiresIn, secret) => {
     let token = await sign(payload, expiresIn, secret);
     return token;
 };
 
- 
+ export const verifyToken = async(token,secret) => {
+    let check = await verifyToken(token, secret);
+    return check;
+ }
--- Server/src/utils/token.utils.js
+++ Server/src/utils/token.utils.js
@@ -14,4 +14,18 @@ export const sign = async (payload, expiresIn, secret) => {
             }
         })
     })
+};
+
+export const verify = async(token,secret)=> {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (error, payload) => {
+            if (error) {
+                logger.error(error);
+                resolve(null);
+            } else {
+                resolve(payload)
+            }
+        })
+    })
 }
+
