83c11042d1706c752e7a0c86d8a214a726d18ed5
Abkels <abrahamk29@gmail.com>
2024-03-05 13:11:46 +0100

    sidebar component

--- FrontEnd/my-app/src/components/sidebar/Sidebar.jsx
+++ FrontEnd/my-app/src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SidebarHeader } from './header'
 import { Notifications } from './notifications'
+import { Search } from './search'
 
 const Sidebar = () => {
+    const [searchResults, setSearchResult] = useState([]);
   return (
     <div className='w-[40%] h-full select-none'>
         {/* Sidebar header */}
         <SidebarHeader />
         {/*Notifications */}
         <Notifications />
+        {/* Search */}
+        <Search searchLength = {searchResults.length}/>
     </div>
   )
 }
