aef199a75410d685e3c0c885a85a33d8fabc08b9
Abkels <abrahamk29@gmail.com>
2024-03-05 12:18:09 +0100

    sidebar notifications

--- FrontEnd/my-app/src/components/sidebar/Sidebar.jsx
+++ FrontEnd/my-app/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { SidebarHeader } from './header'
+import { Notifications } from './notifications'
 
 const Sidebar = () => {
   return (
     <div className='w-[40%] h-full select-none'>
         {/* Sidebar header */}
         <SidebarHeader />
+        {/*Notifications */}
+        <Notifications />
     </div>
   )
 }
