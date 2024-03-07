cc7f23f1611c42a33dadac5b5bd3b43096c31bd1
Abkels <abrahamk29@gmail.com>
2024-03-07 16:59:25 +0100

    sidebar menu and logout functionality

--- FrontEnd/my-app/src/App.js
+++ FrontEnd/my-app/src/App.js
@@ -39,3 +39,51 @@ const App = () => {
 }
 
 export default App
+
+
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+
+// //Pages
+// import Home from "./pages/home";
+// import Login from "./pages/login";
+// import Register from "./pages/register";
+
+
+// function App() {
+//   //const [connected, setConnected] = useState(false);
+//   // const dispatch = useDispatch();
+//   const { user } = useSelector((state) => state.user);
+//   const { token } = user;
+
+//   return (
+//     <div className="dark">
+      
+//         <Router>
+//           <Routes>
+//             <Route
+//               exact
+//               path="/"
+//               element={
+//                 token ? <Home /> : <Navigate to="/login" />
+//               }
+//             />
+//             <Route
+//               exact
+//               path="/login"
+//               element={!token ? <Login /> : <Navigate to="/" />}
+//             />
+//             <Route
+//               exact
+//               path="/register"
+//               element={!token ? <Register /> : <Navigate to="/" />}
+//             />
+//           </Routes>
+//         </Router>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
